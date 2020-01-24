dfcef9df9cfc14ab8c7c58c051657a65194a89b5
Bartosz Gajda <bg@bartoszgajda.com>
2020-01-24 21:50:47 +0000

    Test out sync client in main object.

--- src/main/scala/scalaopenweathermap/ScalaOpenWeatherMap.scala
+++ src/main/scala/scalaopenweathermap/ScalaOpenWeatherMap.scala
@@ -1,18 +1,12 @@
 package scalaopenweathermap
 
-import sttp.client._
+import scalaopenweathermap.client.sync.{SyncClient, SyncClientFacade}
 import scala.io.Source._
 
 object ScalaOpenWeatherMap extends App {
-  val cityName = Some("London")
   val apiKey = fromFile(".env").mkString.substring(7)
 
-  val request = basicRequest
-    .post(uri"https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey")
-
-  implicit val backend = HttpURLConnectionBackend()
-  val response = request.send()
-
-  println(response.body)
-  println(response.headers)
+  val syncClient: SyncClient = SyncClientFacade.getSyncClient(apiKey)
+  val currentWeatherData = syncClient.getCurrentWeatherData("London")
+  println(currentWeatherData)
 }
