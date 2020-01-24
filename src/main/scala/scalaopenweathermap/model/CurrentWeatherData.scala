bab3262c25fd951d281b7f072f249dfcda7f51b4
Bartosz Gajda <bg@bartoszgajda.com>
2020-01-24 21:49:50 +0000

    Remove 'sys' from data model.

--- src/main/scala/scalaopenweathermap/model/CurrentWeatherData.scala
+++ src/main/scala/scalaopenweathermap/model/CurrentWeatherData.scala
@@ -8,10 +8,8 @@ case class Coord(lon: Double,lat: Double)
 
 case class Main(temp: Double, feels_like: Double, temp_min: Double, temp_max: Double, pressure: Int, humidity: Int)
 
-case class RootInterface(coord: Coord, weather: Seq[Weather], base: String, main: Main, visibility: Int, wind: Wind, clouds: Clouds,
-                         dt: Int, sys: Sys, timezone: Int, id: Int, name: String, cod: Int)
-
-case class Sys(`type`: Int, id: Int, message: Double, country: String, sunrise: Int, sunset: Int)
+case class CurrentWeatherData(coord: Coord, weather: Seq[Weather], base: String, main: Main, visibility: Int, wind: Wind, clouds: Clouds,
+                         dt: Int, timezone: Int, id: Int, name: String, cod: Int)
 
 case class Weather(id: Int, main: String, description: String, icon: String)
 
